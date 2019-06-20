55dccf7b53e56cb6acb3d7bc663ea0568b37112f
freddevtsurt101 <fred@tsurt.com.br>
2019-06-19 22:01:14 -0300

    put some pagination

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,18 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\my_user;
 
 class UserController extends Controller
 {
+   private $user;
+
+   public function __construct(My_user $user){
+        
+       $this->user = $user;
+   }
    public function index(){
 
-      return \App\my_user::all();
+        
+       return response()->json($this->user->paginate(5));
+      
     
     //return ['status' => true];
    }
-   public function show( \App\my_user $id){
+   public function show( my_user $id){
+       
+      $data = ['data' => $id];
+       return response()->json($data);
 
-      return $id;
+   }
+
+   public function store(Request $request){
+  
+       dd($request->all());
    }
 }
