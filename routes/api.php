bcf90965e8aa99c97dd0c98f3dc6fed05f06d33d
freddevtsurt101 <fred@tsurt.com.br>
2019-06-19 21:09:12 -0300

    testing the routes with postman

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,37 @@
 <?php
 
 use Illuminate\Http\Request;
+/* I´m using group to avoi write lots of /api/user 
+ I´m not lazzy, but I have a little time today */
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+ Route::group(['prefix' => 'api'], function(){
+
+    Route::group(['prefix'=>'user'], function(){
+          
+         Route::get('', function()
+         {
+             return ' Show list of users';
+         });
+         Route::get('{id}', function($id)
+         {
+             return 'Show users by id '.$id;
+         });
+         Route::post('', function()
+         {
+             return 'Create a new user';
+         });
+         Route::put('{id}', function($id)
+         {
+             return 'Update the user '. $id;
+         });
+         Route::delete('{id}', function($id)
+         {
+             return 'Delete the user '.$id;
+         });
+
+    });
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
